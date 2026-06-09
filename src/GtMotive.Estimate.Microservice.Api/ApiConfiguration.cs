6ee669ab701aecf0f750de8aab82c9050c1c2c99
Zayli Licea Gonzalez <zayli.licea.col@bellaurora.com>
2026-06-09 14:35:02 +0200

    Add base hexagonal architecture template

---
+++ src/GtMotive.Estimate.Microservice.Api/ApiConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using GtMotive.Estimate.Microservice.Api.Authorization;
+using GtMotive.Estimate.Microservice.Api.DependencyInjection;
+using GtMotive.Estimate.Microservice.Api.Filters;
+using GtMotive.Estimate.Microservice.ApplicationCore;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: CLSCompliant(false)]
+
+namespace GtMotive.Estimate.Microservice.Api
+{
+    [ExcludeFromCodeCoverage]
+    public static class ApiConfiguration
+    {
+        public static void ConfigureControllers(MvcOptions options)
+        {
+            ArgumentNullException.ThrowIfNull(options);
+
+            options.Filters.Add<BusinessExceptionFilter>();
+        }
+
+        public static IMvcBuilder WithApiControllers(this IMvcBuilder builder)
+        {
+            ArgumentNullException.ThrowIfNull(builder);
+
+            builder.AddApplicationPart(typeof(ApiConfiguration).GetTypeInfo().Assembly);
+
+            AddApiDependencies(builder.Services);
+
+            return builder;
+        }
+
+        public static void AddApiDependencies(this IServiceCollection services)
+        {
+            services.AddAuthorization(AuthorizationOptionsExtensions.Configure);
+            services.AddMediatR(typeof(ApiConfiguration).GetTypeInfo().Assembly);
+            services.AddUseCases();
+            services.AddPresenters();
+        }
+    }
+}
